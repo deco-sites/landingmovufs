8b593b8871415884e2981eb4fc2223d4bba93ac5
Rômulo Henri <romulohenri000@gmail.com>
2024-06-04 20:21:34 +0000

    update: Landing Page

--- sections/Logos.tsx
+++ sections/Logos.tsx
@@ -29,8 +29,8 @@ export default function Logos({
           <Image
             src={logo.src || ""}
             alt={logo.altText || ""}
-            width={110}
-            height={25}
+            width={48}
+            height={48}
           />
         );
       })}
@@ -40,8 +40,8 @@ export default function Logos({
     <div class="lg:container md:max-w-6xl lg:mx-auto mx-4 py-6 lg:py-14">
       <div class="flex flex-col gap-12">
         <p class="text-center text-lg">{title}</p>
-        <div class="relative w-full overflow-hidden h-11">
-          <div class="animate-sliding absolute top-0 left-0 flex flex-nowrap h-11">
+        <div class="relative w-full overflow-hidden h-32">
+          <div class="animate-sliding absolute top-0 left-0 flex flex-nowrap h-32">
             {slideContent}
           </div>
         </div>
