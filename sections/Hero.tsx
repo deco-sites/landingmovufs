4524118a16dd7982593c033d4781d433b1087c6b
Itamar Rocha Filho <itamardprf@gmail.com>
2024-06-13 22:38:49 -0300

    Simplify hero section

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,5 +1,5 @@
-import type { ImageWidget } from "apps/admin/widgets.ts";
-import Image from "apps/website/components/Image.tsx";
+// import type { ImageWidget } from "apps/admin/widgets.ts";
+// import Image from "apps/website/components/Image.tsx";
 
 export interface CTA {
   id?: string;
@@ -17,8 +17,8 @@ export interface Props {
   /**
    * @default This text is fully editable and ready for your personal touch. Just click here, head over to the section window, or dive straight into the code to make changes as you see fit. Whether it's about the content, formatting, font, or anything in between, editing is just a click away.
    */
-  description?: string;
-  image?: ImageWidget;
+  // description?: string;
+  // image?: ImageWidget;
   placement?: "left" | "right";
   cta?: CTA[];
 }
@@ -30,9 +30,9 @@ const PLACEMENT = {
 
 export default function HeroFlats({
   title = "Click here to tweak this text however you want.",
-  description =
-    "This text is fully editable and ready for your personal touch. Just click here, head over to the section window, or dive straight into the code to make changes as you see fit. Whether it's about the content, formatting, font, or anything in between, editing is just a click away.",
-  image,
+  // description =
+  //   "This text is fully editable and ready for your personal touch. Just click here, head over to the section window, or dive straight into the code to make changes as you see fit. Whether it's about the content, formatting, font, or anything in between, editing is just a click away.",
+  // image,
   placement = "left",
   cta = [
     { id: "change-me-1", href: "/", text: "Change me", outline: false },
@@ -43,40 +43,18 @@ export default function HeroFlats({
     <nav class="lg:container lg:mx-auto mx-4">
       <div class="flex flex-col items-center gap-8">
         <div
-          class={`flex w-full xl:container xl:mx-auto py-20 mx-5 md:mx-10 z-10 ${
-            image
-              ? PLACEMENT[placement]
-              : "flex-col items-center justify-center text-center"
-          } lg:py-36 gap-12 md:gap-20 items-center`}
+          class={`flex w-full xl:container xl:mx-auto py-20 mx-5 md:mx-10 z-10 flex-col items-center justify-center text-center lg:py-36 gap-12 md:gap-20 items-center`}
         >
-          {image && (
-            <Image
-              width={640}
-              class="w-full lg:w-1/2 object-fit"
-              sizes="(max-width: 640px) 100vw, 30vw"
-              src={image}
-              alt={image}
-              decoding="async"
-              loading="lazy"
-            />
-          )}
           <div
-            class={`mx-6 lg:mx-auto lg:w-full space-y-4 gap-4 ${
-              image
-                ? "lg:w-1/2 lg:max-w-xl"
-                : "flex flex-col items-center justify-center lg:max-w-3xl"
-            }`}
+            class={`mx-6 lg:mx-auto lg:w-full space-y-4 gap-4 flex flex-col items-center justify-center lg:max-w-3xl`}
           >
             <div
-              class="inline-block lg:text-[80px] text-4xl leading-none font-medium"
+              class="inline-block lg:text-[80px] text-4xl text-primary leading-none font-medium"
               dangerouslySetInnerHTML={{
                 __html: title,
               }}
             >
             </div>
-            <p class="text-lg md:text-md leading-[150%]">
-              {description}
-            </p>
             <div class="flex items-center gap-3">
               {cta?.map((item) => (
                 <a
