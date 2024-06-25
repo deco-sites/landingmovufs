c36751feae29b3cc5a69bea2be708e495c12099c
Igor Brasileiro <igor@deco.cx>
2024-06-25 16:46:54 +0000

    Setup deco records

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -4,8 +4,9 @@
 
 import * as $$$$$$$$$$$0 from "./apps/deco/blog.ts";
 import * as $$$$$$$$$$$1 from "./apps/deco/htmx.ts";
-import * as $$$$$$$$$$$2 from "./apps/decohub.ts";
-import * as $$$$$$$$$$$3 from "./apps/site.ts";
+import * as $$$$$$$$$$$2 from "./apps/deco/records.ts";
+import * as $$$$$$$$$$$3 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$4 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/AlumniGrid.tsx";
 import * as $$$$$$1 from "./sections/BlogPosts.tsx";
 import * as $$$$$$2 from "./sections/Faq.tsx";
@@ -49,8 +50,9 @@ const manifest = {
   "apps": {
     "site/apps/deco/blog.ts": $$$$$$$$$$$0,
     "site/apps/deco/htmx.ts": $$$$$$$$$$$1,
-    "site/apps/decohub.ts": $$$$$$$$$$$2,
-    "site/apps/site.ts": $$$$$$$$$$$3,
+    "site/apps/deco/records.ts": $$$$$$$$$$$2,
+    "site/apps/decohub.ts": $$$$$$$$$$$3,
+    "site/apps/site.ts": $$$$$$$$$$$4,
   },
   "name": "site",
   "baseUrl": import.meta.url,
