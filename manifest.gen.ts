2e0ffec1aef0b6a70b714f68559bb29fc385e26c
Itamar Rocha Filho <itamardprf@gmail.com>
2024-06-25 20:16:20 -0300

    Fix format, add resend

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -5,8 +5,9 @@
 import * as $$$$$$$$$$$0 from "./apps/deco/blog.ts";
 import * as $$$$$$$$$$$1 from "./apps/deco/htmx.ts";
 import * as $$$$$$$$$$$2 from "./apps/deco/records.ts";
-import * as $$$$$$$$$$$3 from "./apps/decohub.ts";
-import * as $$$$$$$$$$$4 from "./apps/site.ts";
+import * as $$$$$$$$$$$3 from "./apps/deco/resend.ts";
+import * as $$$$$$$$$$$4 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$5 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/AlumniGrid.tsx";
 import * as $$$$$$1 from "./sections/BlogPosts.tsx";
 import * as $$$$$$2 from "./sections/Faq.tsx";
@@ -51,8 +52,9 @@ const manifest = {
     "site/apps/deco/blog.ts": $$$$$$$$$$$0,
     "site/apps/deco/htmx.ts": $$$$$$$$$$$1,
     "site/apps/deco/records.ts": $$$$$$$$$$$2,
-    "site/apps/decohub.ts": $$$$$$$$$$$3,
-    "site/apps/site.ts": $$$$$$$$$$$4,
+    "site/apps/deco/resend.ts": $$$$$$$$$$$3,
+    "site/apps/decohub.ts": $$$$$$$$$$$4,
+    "site/apps/site.ts": $$$$$$$$$$$5,
   },
   "name": "site",
   "baseUrl": import.meta.url,
